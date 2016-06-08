efcc2361d0e63610eb120acd247d20d9686e357b
Ian W <ian.uu12@gmail.com>
2016-06-07 18:13:56 -0700

    File selector to load data files

--- src/io/github/projectchroma/analytics/gui/ContentArea.java
+++ src/io/github/projectchroma/analytics/gui/ContentArea.java
@@ -5,8 +5,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 
 import io.github.projectchroma.analytics.Analytics;
@@ -17,13 +19,16 @@ import io.github.projectchroma.launcher.gui.util.Spacer;
 public class ContentArea extends BaseComponent{
 	private static final long serialVersionUID = 1L;
 	private JTextField pathField = new JTextField();
+	private JFileChooser fileSelector = new JFileChooser(new File("").getAbsoluteFile());
+	private File gameDir;
 	public ContentArea(){
 		super(new GridBagLayout(), Color.white, Color.black);
+		fileSelector.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		
 		GridBagConstraints c = new GridBagConstraints();
 		c.anchor = GridBagConstraints.NORTH;
 		c.fill = GridBagConstraints.BOTH;
-		c.gridwidth = 2;
+		c.gridwidth = 3;
 		c.gridheight = 1;
 		c.gridx = c.gridy = 0;
 		c.weightx = 1;
@@ -31,27 +36,69 @@ public class ContentArea extends BaseComponent{
 		add(new Header(), c);
 		
 		c.gridy++;
+		c.weighty = 0;
+		JTextField fileCaption = new JTextField("Game directory");
+		fileCaption.setEditable(false);
+		fileCaption.setFocusable(false);
+		fileCaption.setBackground(null);
+		fileCaption.setBorder(null);
+		fileCaption.setFont(fileCaption.getFont().deriveFont(20F));
+		add(fileCaption, c);
+		
+		c.gridy++;
 		c.gridwidth = 1;
 		c.weightx = 9;
 		c.weighty = 1;
-		pathField.setFont(Analytics.getFont(24));
+		pathField.setFont(pathField.getFont().deriveFont(16F));
+		pathField.setEditable(false);
+		pathField.setFocusable(false);
 		add(pathField, c);
 		
 		c.gridx++;
-		c.weightx = 1;
+		c.weightx = 2;
 		JButton browseButton = new JButton("Browse");
 		add(browseButton, c);
 		browseButton.addMouseListener(new MouseAdapter(){
 			public void mouseReleased(MouseEvent e){
 				Analytics.log().write("Browsing", Log.DEBUG);
+				int code = fileSelector.showOpenDialog(ContentArea.this);
+				if(code == JFileChooser.APPROVE_OPTION){
+					gameDir = fileSelector.getSelectedFile();
+					Analytics.log().write("Selected file " + gameDir, Log.DEBUG);
+					pathField.setText(gameDir.toString());
+				}else{
+					Analytics.log().write("Browsing canceled", Log.DEBUG);
+				}
+			}
+		});
+		
+		c.gridx++;
+		c.weightx = 0.5;
+		JButton loadButton = new JButton("Load Data");
+		add(loadButton, c);
+		loadButton.addMouseListener(new MouseAdapter(){
+			public void mouseReleased(MouseEvent e){
+				Analytics.log().write("Loading data", Log.DEBUG);
+				if(gameDir == null){
+					Analytics.log().write("No game dir selected", Log.ERROR);
+					return;
+				}
+				File dataDir = new File(gameDir, "data" + File.separatorChar + "analytics");
+				if(!dataDir.exists()){
+					Analytics.log().write("Unable to load data: analytics data folder " + dataDir + " does not exist", Log.ERROR);
+					return;
+				}
+				for(File file : dataDir.listFiles((dir, name) -> name.matches("\\d+\\.dat"))){
+					Analytics.log().write("Loading file " + file, Log.DEBUG);
+				}
 			}
 		});
 		
 		c.gridx = 0;
 		c.gridy++;
-		c.gridwidth = 2;
+		c.gridwidth = 3;
 		c.weightx = 1;
-		c.weighty = 19;
+		c.weighty = 24;
 		add(new Spacer(1, false), c);
 	}
 }

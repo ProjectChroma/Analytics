a06bcc5af64a8778963720538770da74f90801ab
Ian W <ian.uu12@gmail.com>
2016-06-09 18:08:09 -0700

    Draw keys and grid lines

--- src/io/github/projectchroma/analytics/gui/GraphArea.java
+++ src/io/github/projectchroma/analytics/gui/GraphArea.java
@@ -13,7 +13,7 @@ import io.github.projectchroma.launcher.gui.util.BaseComponent;
 
 public class GraphArea extends BaseComponent{
 	private static final long serialVersionUID = 1L;
-	private static final int AXIS_WIDTH = 5;
+	private static final int AXIS_WIDTH = 5, KEY_WIDTH = 50;
 	private List<Point> points = new ArrayList<>();
 	private int graphWidth = 0, graphHeight = 0;
 	public GraphArea(){
@@ -22,18 +22,38 @@ public class GraphArea extends BaseComponent{
 	@Override
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.fillRect(0, 0, AXIS_WIDTH, getHeight());
-		g.fillRect(0, getHeight()-AXIS_WIDTH, getWidth(), AXIS_WIDTH);
+		//Draw axes
+		g.fillRect(KEY_WIDTH, 0, AXIS_WIDTH, getHeight()-KEY_WIDTH);
+		g.fillRect(KEY_WIDTH, getHeight()-AXIS_WIDTH-KEY_WIDTH, getWidth()-KEY_WIDTH, AXIS_WIDTH);
 		
 		if(graphWidth == 0 || graphHeight == 0) return;
 		
-		int height = getHeight() - AXIS_WIDTH, xScale = getWidth() / graphWidth, yScale = height / graphHeight;
+		int width = getWidth() - (KEY_WIDTH + AXIS_WIDTH), height = getHeight() - (AXIS_WIDTH + KEY_WIDTH), xScale = width / graphWidth, yScale = height / graphHeight;
 		Analytics.log().write("Scale: " + xScale + "x" + yScale, Log.DEBUG);
+		
+		//Draw vertical key
+		for(int y=0; y<=graphHeight; ++y){
+			g.drawString(y + "", KEY_WIDTH/2, (graphHeight - y) * yScale + getFontMetrics(getFont()).getHeight()/2);
+		}
+		//Draw horizontal key
+		for(int x=0; x<=graphWidth; ++x){
+			g.drawString(x + "", x * xScale - getFontMetrics(getFont()).stringWidth(x + "")/2 + KEY_WIDTH, getHeight() - (KEY_WIDTH + getFontMetrics(getFont()).getHeight()) / 2);
+		}
+		//Draw grid rows
+		for(int y=0; y<=graphHeight; ++y){
+			g.drawLine(KEY_WIDTH + AXIS_WIDTH, y * yScale, getWidth(), y * yScale);
+		}
+		//Draw grid columns
+		for(int x=0; x<=graphWidth; ++x){
+			g.drawLine(KEY_WIDTH + AXIS_WIDTH + x * xScale, 0, KEY_WIDTH + AXIS_WIDTH + x * xScale, height);
+		}
 		
 		g.setColor(Color.yellow.darker());
 		Point prev = points.get(0);
 		for(Point p : points){
-			g.drawLine(prev.x * xScale, height - prev.y * yScale, p.x * xScale, height - p.y * yScale);
+			for(int i=-4; i<=0; i++){
+				g.drawLine(prev.x * xScale + KEY_WIDTH + AXIS_WIDTH, height - prev.y * yScale + i, p.x * xScale + KEY_WIDTH + AXIS_WIDTH, height - p.y * yScale + i);
+			}
 			prev = p;
 		}
 	}
